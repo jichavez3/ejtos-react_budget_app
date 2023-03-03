aa490b818018b6c9129904d8e9ddc7623b437f5d
Jason <jason.chavez.3@gmail.com>
2023-03-03 16:46:39 -0500

    Added the decrease button, should be the final check in

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -56,7 +56,7 @@ const AllocationForm = (props) => {
                 </div>
                   <select className="custom-select" id="inputGroupSelect02" onChange={(event) => setAction(event.target.value)}>
                         <option defaultValue value="Add" name="Add">Add</option>
-                <option value="Reduce" name="Reduce">Reduce</option>
+                        <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
                   <span class="eco" style={{ marginLeft: '2rem', marginRight: '4px'}}>{currency}</span>
 
